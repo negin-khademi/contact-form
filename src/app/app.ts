b809ff4b61a43aead3feb2f4da2306c3a595c856
negin-khademi <negin.khademi@gmail.com>
2025-08-25 01:43:43 +0330

    feat : put form in the center

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from "@angular/core";
 
+import { ContactForm } from "./contact-form/contact-form";
+
 @Component({
   selector: "app-root",
-  imports: [],
+  imports: [ContactForm],
   templateUrl: "./app.html",
   styleUrl: "./app.scss",
 })
